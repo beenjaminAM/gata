ebe99745eb5c46f81a666769d0fe2f7b7658a46a
beenjaminAM <benjamin.ancaya@unmsm.edu.pe>
2025-08-11 17:17:33 -0500

    match date time

--- script/ib.py
+++ script/ib.py
@@ -91,8 +91,8 @@ def run():
                         events_array = events.map(data => {
                             let event_details = data.querySelector('a.obg-event-row-details'); // match details (teams and time)
                             let participants_names = Array.from(event_details.querySelectorAll('.obg-event-scorecard-participants-name')).map(el => el.innerText);
-                            let parts_odds = Array.from(data.querySelectorAll('.obg-selection-base.genos-interactive.ng-star-inserted')).map(el => el.innerText.replace('\\n', '-'))
-                            return {"date": events_date, "parts": participants_names, "odds": parts_odds}
+                            let date_time = event_details.querySelector('time').innerText // will return null on live matches
+                            
                         })
                         return events_array
                     })
