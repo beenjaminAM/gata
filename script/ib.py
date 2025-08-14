d8790f5e68b9b3474619dbf75f2733dab16f7ad5
beenjaminAM <benjamin.ancaya@unmsm.edu.pe>
2025-08-13 19:42:41 -0500

    prevent null err

--- script/ib.py
+++ script/ib.py
@@ -110,8 +110,8 @@ def run():
                             under_goals_market_odds = betting[1].querySelectorAll('.obg-selection-base-odds')[1]?.innerText
                             betting[2] // european handicap
                             betting[3] // both teams score
-                            let both_teams_to_score_yes = betting[3].querySelectorAll('.obg-selection-base-odds')[0].innerText
-                            let both_teams_to_score_no  = betting[3].querySelectorAll('.obg-selection-base-odds')[1].innerText
+                            let both_teams_to_score_yes = betting[3].querySelectorAll('.obg-selection-base-odds')[0]?.innerText
+                            let both_teams_to_score_no  = betting[3].querySelectorAll('.obg-selection-base-odds')[1]?.innerText
                             let matchObject = {
                                 date_time: date_time,
                                 home_team: home_team,
